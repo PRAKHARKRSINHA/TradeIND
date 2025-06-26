34863805752a05823195801a84a0ea5eac7b24c0
prakhar sinha <prakharkumarsinha8@gmail.com>
2025-06-26 16:28:34 +0530

    initial commit

--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -4,8 +4,8 @@ import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 
 const Signup = () => {
-  const toDashboard = () => {
-    window.location.href = "https://zerodha-dashboard-76qo.onrender.com/";
+  const toLogin = () => {
+    window.location.href = "https://zerodha-clone-1-w0i1.onrender.com/login";
   };
   const [inputValue, setInputValue] = useState({
     email: "",
@@ -49,7 +49,7 @@ const Signup = () => {
       if (success) {
         handleSuccess(message);
         setTimeout(() => {
-          toDashboard();
+          toLogin();
         }, 1000);
       } else {
         handleError(message);
@@ -69,68 +69,71 @@ const Signup = () => {
   return (
     <div className="container p-3 p-sm-5 text-center">
       <div className="center-wrapper">
-      <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-12 col-sm-7">
-            <img
-              src="assets\signupPageIMG.png"
-              alt=""
-              style={{ width: "100%" }}
-            />
-          </div>
-          <div className="col-sm-5 p-3 p-sm-5 pt-sm-0">
-            <p className="blockquote-footer" style={{ textAlign: "center" }}>
-              <em>if you are a visitor and dont want to signup</em>
-            </p>
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={toDashboard}
-            >
-              Guest Login
-            </button>
-            <hr />
-            <h2 className="text-center">Signup</h2>
-            <div className="d-flex justify-content-center">
-              <div className="mt-4 text-center">
-                <input
-                  className="mb-4 p-2 text-muted"
-                  type="email"
-                  name="email"
-                  value={email}
-                  placeholder="Email"
-                  onChange={handleOnChange}
-                />
-                <br />
-                <input
-                  className="mb-4 p-2 text-muted"
-                  type="text"
-                  name="username"
-                  value={username}
-                  placeholder="Username"
-                  onChange={handleOnChange}
-                />
-                <br />
-                <input
-                  className="mb-4 p-2 text-muted"
-                  type="password"
-                  name="password"
-                  value={password}
-                  placeholder="Password"
-                  onChange={handleOnChange}
-                />
-                <br />
-                <button type="submit" className="mx-auto btn btn-primary mb-5">
-                  Submit
-                </button>
-                <p>
-                  Already have an account? <Link to={"/login"}>Login</Link>
-                </p>
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-12 col-sm-7">
+              <img
+                src="assets\signupPageIMG.png"
+                alt=""
+                style={{ width: "100%" }}
+              />
+            </div>
+            <div className="col-sm-5 p-3 p-sm-5 pt-sm-0">
+              <p className="blockquote-footer" style={{ textAlign: "center" }}>
+                <em>if you are a visitor and dont want to signup</em>
+              </p>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={toLogin}
+              >
+                Guest Login
+              </button>
+              <hr />
+              <h2 className="text-center">Signup</h2>
+              <div className="d-flex justify-content-center">
+                <div className="mt-4 text-center">
+                  <input
+                    className="mb-4 p-2 text-muted"
+                    type="email"
+                    name="email"
+                    value={email}
+                    placeholder="Email"
+                    onChange={handleOnChange}
+                  />
+                  <br />
+                  <input
+                    className="mb-4 p-2 text-muted"
+                    type="text"
+                    name="username"
+                    value={username}
+                    placeholder="Username"
+                    onChange={handleOnChange}
+                  />
+                  <br />
+                  <input
+                    className="mb-4 p-2 text-muted"
+                    type="password"
+                    name="password"
+                    value={password}
+                    placeholder="Password"
+                    onChange={handleOnChange}
+                  />
+                  <br />
+                  <button
+                    type="submit"
+                    className="mx-auto btn btn-primary mb-5"
+                  >
+                    Submit
+                  </button>
+                  <p>
+                    Already have an account? <Link to={"/login"}>Login</Link>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </form>
+        </form>
       </div>
       <ToastContainer />
     </div>
